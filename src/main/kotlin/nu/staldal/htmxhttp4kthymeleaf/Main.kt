621c46c7ac2faede93bb8a14b6926d01a55d5403
Mikael Ståldal <mikael@staldal.nu>
2024-11-10 20:12:40 +0100

    Fix css

--- src/main/kotlin/nu/staldal/htmxhttp4kthymeleaf/Main.kt
+++ src/main/kotlin/nu/staldal/htmxhttp4kthymeleaf/Main.kt
@@ -433,6 +433,7 @@ fun main() {
         static(Classpath("static")),
         htmxWebjars(),
         webjar("bootstrap", "5.3.3"),
+        webjar("missing.css", "1.1.3"),
         webjar("sweetalert2", "11.12.3"),
     )
 
